c0a0b97099b69b02271d1f36d3976e81739249a1
magnus2142 <magnus.bredeli@hotmail.com>
2021-02-25 21:58:45 +0100

    fixed code

--- oblig-2-UDP-TLS-ASIO/UDP/UDPClient.java
+++ oblig-2-UDP-TLS-ASIO/UDP/UDPClient.java
@@ -48,11 +48,7 @@ public class UDPClient {
             buffer = new byte[512];
             DatagramPacket calcResponsePacket = new DatagramPacket(buffer, buffer.length);
             socket.receive(calcResponsePacket);
-            try {
-                Integer.parseInt(new String(calcResponsePacket.getData(), 0, calcResponsePacket.getLength()));
-            } catch (Exception e) {
-                running = false;
-            }
+
             String calcAnswer = new String(calcResponsePacket.getData(), 0, calcResponsePacket.getLength());
             System.out.println(calcAnswer);
         }
